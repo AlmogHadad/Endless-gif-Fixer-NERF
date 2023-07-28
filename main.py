e53ec12dc80dd05f5b053d8a840e628f7d5b189e
Almog Hadad <almoghadad11@gmail.com>
2023-07-28 09:43:47 +0300

    modified:   .idea/.name 	new file:   .idea/vcs.xml 	modified:   main.py

--- main.py
+++ main.py
@@ -1,22 +1,26 @@
 import subprocess
-# import gif_handler
-# import wget
-import numpy as np
-import os
+import gif_handler
 
-# gif_input = "example.gif"
-# output_prefix = "frame"
-#
-# frame_durations = gif_handler.create_images_from_gif(gif_input, output_prefix)
-#
-# # Example usage
-# folder_path = "./frames"
-# output_path = "output.gif"
-# duration = frame_durations  # Duration in milliseconds per frame
-# loop = 0  # 0 means loop indefinitely, any other number represents the number of loops
-#
-# gif_handler.create_gif_from_images(folder_path, output_path, duration, loop)
-print("hi")
+gif_input = "example.gif"
+output_prefix = "frame"
+
+# step 1 - from GIF input, make set of images
+gif_handler.create_images_from_gif(gif_input, output_prefix)
+
+# step 2 - from the set of images, play COLMAP
+subprocess.run(['python', './COLMAP_executable.py'])
+
+# step 3 - from COLMAP output, process the data to input it to the NERF network
 subprocess.run(['python', './colmap2nerf.py'])
-# subprocess.run(['python', './data_preparation.py'])
-# subprocess.run(['python', './find_missing_posses.py'])
+
+# step 4 - train NERF network (instant-ngp)
+# - to import the instant-ngp as subprocess
+
+# step 5 - find the missing posses
+subprocess.run(['python', './find_missing_posses.py'])
+
+# step 6 - extract missing posses from the NERF
+subprocess.run(['python', './extract_posses.py'])
+
+# step 7 - create gif from the set of images + new frames
+gif_handler.create_gif_from_images(gif_input, output_prefix)
