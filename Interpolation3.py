3a2cdca20cd9697d8c7be1a31b87298eaff6f959
Y Patterns <85457983+YaadR@users.noreply.github.com>
2023-09-10 23:54:53 +0300

    Relative noise addition

--- Interpolation3.py
+++ Interpolation3.py
@@ -7,8 +7,9 @@ import json
 import os
 from random import random
 
-
 GAP_EDGE = 10
+
+
 def circle_data():
     # Parameters of the circle
     radius = 1.0
@@ -21,37 +22,17 @@ def circle_data():
     z = np.random.uniform(-0.2, 0.2, num_points)  # Random z values in the range [-0.1, 0.1]
 
     circle = np.column_stack([x, y, z])
-    circle = circle[2*GAP_EDGE:]
+    circle = circle[2 * GAP_EDGE:]
     return circle
-
-
-# Define the neural network architecture
-class NonlinearApproximator(nn.Module):
-    def __init__(self):
-        super(NonlinearApproximator, self).__init__()
-        self.hidden_layers = [18,24]
-        self.fc1 = nn.Linear(2, self.hidden_layers[0])
-        self.fc2 = nn.Linear(self.hidden_layers[0], self.hidden_layers[1])
-        self.fc3 = nn.Linear(self.hidden_layers[1], 3)  # Output has 2 dimensions for y and z
-
-
-    def forward(self, x):
-        x = self.fc1(x)
-        x = torch.sigmoid(x)
-        x = self.fc2(x)
-        x = torch.sigmoid(x)
-        output = self.fc3(x)
-        return output
 
 # Generate synthetic data for demonstration
 def generate_data():
     data = circle_data()
-    data = torch.tensor(data,dtype=torch.float32)
-    x_data =  data[:,0].unsqueeze(0).T
-    y_data =  data[:,1].unsqueeze(0).T
-    z_data =  data[:,2].unsqueeze(0).T
+    data = torch.tensor(data, dtype=torch.float32)
+    x_data = data[:, 0].unsqueeze(0).T
+    y_data = data[:, 1].unsqueeze(0).T
+    z_data = data[:, 2].unsqueeze(0).T
     return x_data, y_data, z_data
-
 
 
 def frame_rate_function(points_matrix):
@@ -65,7 +46,6 @@ def frame_rate_function(points_matrix):
     average_distance = total_distance / num_pairs
     gap_distance = np.linalg.norm(points_matrix[0] - points_matrix[-1])
     return int(np.ceil(gap_distance / average_distance))
-
 
 
 def plot_camera_positions_with_direction(json_data):
@@ -96,21 +76,11 @@ def plot_camera_positions_with_direction(json_data):
     transform_matrix_first_frame = np.array(data['frames'][0]['transform_matrix'])
     transform_matrix_last_frame = np.array(data['frames'][-1]['transform_matrix'])
 
-    return np.column_stack((camera_positions_x, camera_positions_y, camera_positions_z)),transform_matrix_first_frame,transform_matrix_last_frame
+    return np.column_stack((camera_positions_x, camera_positions_y,
+                            camera_positions_z)), transform_matrix_first_frame, transform_matrix_last_frame
 
 
 def filter_points_between(p1, p2, points):
-    """
-    Filter the points that are between p1 and p2.
-
-    Parameters:
-        p1 (np.ndarray): First 3D point.
-        p2 (np.ndarray): Second 3D point.
-        points (np.ndarray): Array of 3D points.
-
-    Returns:
-        np.ndarray: Filtered array of points.
-    """
     v1 = p2 - p1
     v2 = points - p1
     dot_product = np.sum(v1 * v2, axis=1)
@@ -118,16 +88,29 @@ def filter_points_between(p1, p2, points):
     filtered_points = points[(projection >= 0) & (projection <= 1)]
     return filtered_points
 
-# # Create the model, loss function, and optimizer
-model = NonlinearApproximator()
-criterion = nn.MSELoss()
-optimizer = optim.Adam(model.parameters(), lr=0.01)
+def noise_immitation(num_of_frames,x_new,y_new,z_new):
+    axis_std = []
+    for i in range(0, len(data), ):
+        partial_std = np.std(data[i:i + num_of_frames], axis=0)
+        axis_std.append(partial_std)
+    axis_std = np.mean(axis_std, axis=0)
+    # Set the mean and standard deviation of the normal distribution
+    mean = 0
+    std_deviation = 1
+    # Generate a single random number with a normal distribution
+    for i in range(len(x_new)):
+        random_number = np.random.normal(mean, std_deviation, 3)
+        x_new[i] = x_new[i] + random_number[0] * axis_std[0] * .7
+        y_new[i] = y_new[i] + random_number[1] * axis_std[1] * .7
+        z_new[i] = z_new[i] + random_number[2] * axis_std[2] * .7
+
+    return x_new, y_new, z_new
 
 PATH = r'data_example\SandBox\transforms2.json'
 with open(PATH, 'r') as file:
     # Initialize an empty list to store the rows of data
     mat = file.read()
-    data,transform_matrix_first_frame,transform_matrix_last_frame = plot_camera_positions_with_direction(mat)
+    data, transform_matrix_first_frame, transform_matrix_last_frame = plot_camera_positions_with_direction(mat)
 
 # data = circle_data()
 
@@ -137,7 +120,6 @@ start_p = data[0]
 end_p = data[-1]
 points_left = data[:GAP_EDGE]
 points_right = data[-GAP_EDGE:]
-
 
 
 class NonlinearApproximator(nn.Module):
@@ -156,23 +138,15 @@ class NonlinearApproximator(nn.Module):
         output = self.fc3(x)
         return output
 
-# Generate synthetic data for demonstration
-def generate_data():
-    time = np.linspace(0, 10, 100)  # Generate time values
-    x = 2 * np.sin(time)  # Replace with your own function
-    y = 3 * np.cos(time)  # Replace with your own function
-    z = np.random.uniform(-0.2, 0.2, len(time))  # Random z values in the range [-0.1, 0.1]
-    return time, np.column_stack([x, y, z])
 
 # Create the model, loss function, and optimizer
 model = NonlinearApproximator()
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.01)
 
-# Generate synthetic data
 # time_data, target_data = generate_data()
 target_data = np.concatenate((points_right, points_left))
-time_data = [i for i in range(1,GAP_EDGE+1)] + [j +num_intermediate_frames for j in range(GAP_EDGE,2*GAP_EDGE)]
+time_data = [i for i in range(1, GAP_EDGE + 1)] + [j + num_intermediate_frames for j in range(GAP_EDGE, 2 * GAP_EDGE)]
 time_data = torch.tensor(time_data, dtype=torch.float32).unsqueeze(1)  # Convert to 2D tensor
 target_data = torch.tensor(target_data, dtype=torch.float32)
 
@@ -185,7 +159,7 @@ for epoch in range(num_epochs):
     loss.backward()
     optimizer.step()
     if (epoch + 1) % 10 == 0:
-        print(f'Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.4f}')
+        print(f'Epoch [{epoch + 1}/{num_epochs}], Loss: {loss.item():.4f}')
 
 # Test the model
 with torch.no_grad():
@@ -193,10 +167,13 @@ with torch.no_grad():
     x_fit, y_fit, z_fit = predicted_xyz[:, 0], predicted_xyz[:, 1], predicted_xyz[:, 2]
 
 with torch.no_grad():
-    new_time_data = [i+GAP_EDGE for i in range(num_intermediate_frames)]
+    new_time_data = [i + GAP_EDGE for i in range(num_intermediate_frames)]
     new_time_data = torch.tensor(new_time_data, dtype=torch.float32).unsqueeze(1)  # Convert to 2D tensor
     predicted_xyz = model(new_time_data)
     x_new, y_new, z_new = predicted_xyz[:, 0], predicted_xyz[:, 1], predicted_xyz[:, 2]
+
+x_new, y_new, z_new = noise_immitation(num_intermediate_frames,x_new,y_new,z_new)
+
 
 # Plot the original data points and the predicted points
 plt.plot(time_data, target_data[:, 0], label='True X')
@@ -225,7 +202,6 @@ ax.set_zlabel('Z')
 ax.set_title('Polynomial Regression')
 plt.show()
 
-
 with open('./data_example/SandBox/transforms2.json', 'r') as file:
     json_data = file.read()
 
@@ -240,10 +216,12 @@ interpolated_frames = []
 # Append the intermediate frame with its transformation matrix to the new data
 for t in np.linspace(0, 1, num_intermediate_frames + 2)[1:-1]:  # Exclude the endpoints (0 and 1)
     # Interpolate rotation matrix
-    rotation_matrix_interpolated = (1 - t) * transform_matrix_first_frame[:3, :3] + t * transform_matrix_last_frame[:3, :3]
+    rotation_matrix_interpolated = (1 - t) * transform_matrix_first_frame[:3, :3] + t * transform_matrix_last_frame[:3,
+                                                                                        :3]
 
     # Interpolate translation vector
-    translation_vector_interpolate = (1 - t) * transform_matrix_first_frame[:3, 3] + t * transform_matrix_last_frame[:3, 3]
+    translation_vector_interpolate = (1 - t) * transform_matrix_first_frame[:3, 3] + t * transform_matrix_last_frame[:3,
+                                                                                         3]
 
     # Combine rotation and translation into a single transformation matrix
     intermediate_pose_matrix = np.eye(4)
---
+++ SandBox.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+# Set the mean and standard deviation of the normal distribution
+mean = 0
+std_deviation = 1
+
+# Generate a single random number with a normal distribution
+random_number = np.random.normal(mean, std_deviation)
+
